86b86bfdca10679a2e1bdd2dcfd57c0431e1f97c
Minh Lu Luan <minhlu14206@gmail.com>
2024-10-25 15:47:19 +0200

    change homepage urls

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,18 @@
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import Home from '../pages/home'
-import AboutPage from '../pages/about';
-import ProjectPage from '../pages/project';
-import PortfolioProjects from '../portfolio-Projects/portfolio-Projects'
-import ContactPage from '../pages/contact';
-
+// App.jsx
+import { Outlet } from 'react-router-dom';
 import { ChangePageProvider } from '../contextAPI/changePageContext';
 
-function App() {
-  
 
+function App() {
   return (
-    <>
-      <ChangePageProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route index element={<Home/>}/>
-            <Route path='/Home' element={<Home/>}/>
-            <Route path='/About' element={<AboutPage/>}/>
-            <Route path='/Project' element={<ProjectPage/>}/>
-            <Route path='/PortfolioProjects' element={<PortfolioProjects/>}/>
-            <Route path='/Contact' element={<ContactPage/>}/>
-          </Routes>
-        </BrowserRouter>
-      </ChangePageProvider>
-    </>
-  )
+    <ChangePageProvider>
+      <div>
+        <main>
+          <Outlet /> {/* This is where child routes will render */}
+        </main>
+      </div>
+    </ChangePageProvider>
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+// main.jsx
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
 
+import { createBrowserRouter, RouterProvider, createRoutesFromElements, Route } from 'react-router-dom';
+import Home from '../pages/home.jsx';
+import AboutPage from '../pages/about.jsx';
+import ProjectPage from '../pages/project.jsx';
+import ContactPage from '../pages/contact.jsx';
+import PortfolioProjects from '../portfolio-Projects/portfolio-Projects.jsx'
+
+// Create the router with all routes
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/Resume-ReactJS/' element={<App />}>
+      <Route index element={<Home />} />
+      <Route path='about' element={<AboutPage />} />
+      <Route path='project' element={<ProjectPage />} />
+      <Route path='portfolioProjects' element={<PortfolioProjects />} />
+      <Route path='contact' element={<ContactPage />} />      
+    </Route>
+  )
+);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
