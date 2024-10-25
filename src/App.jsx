c62575d60eea509594df8915714775a263fd3985
Minh Lu Luan <minhlu14206@gmail.com>
2024-10-25 07:56:36 +0200

    Adjust the Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,30 @@
-import {BrowserRouter, Route, Routes, HashRouter as Router, Switch } from 'react-router-dom';
-import Home from '../pages/home'
+import { HashRouter as Router, Route, Routes } from 'react-router-dom'; // Use HashRouter for GitHub Pages
+import Home from '../pages/home';
 import AboutPage from '../pages/about';
 import ProjectPage from '../pages/project';
-import PortfolioProjects from '../portfolio-Projects/portfolio-Projects'
+import PortfolioProjects from '../portfolio-Projects/portfolio-Projects';
 import ContactPage from '../pages/contact';
 
 import { ChangePageProvider } from '../contextAPI/changePageContext';
 
 function App() {
-  
-
   return (
     <>
       <ChangePageProvider>
+        {/* Set basename for GitHub Pages */}
         <Router basename='/Resume-ReactJS'>
-          <Switch>
-            <Route exact path="/" component={HomePage} />
-            <Route index element={<Home/>}/>
-            <Route path='/Home' element={<Home/>}/>
-            <Route path='/About' element={<AboutPage/>}/>
-            <Route path='/Project' element={<ProjectPage/>}/>
-            <Route path='/PortfolioProjects' element={<PortfolioProjects/>}/>
-            <Route path='/Contact' element={<ContactPage/>}/>
-          </Switch>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/Home" element={<Home />} />
+            <Route path="/About" element={<AboutPage />} />
+            <Route path="/Project" element={<ProjectPage />} />
+            <Route path="/PortfolioProjects" element={<PortfolioProjects />} />
+            <Route path="/Contact" element={<ContactPage />} />
+          </Routes>
         </Router>
       </ChangePageProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
